51bf38553108e420a0a4e351439993a8ec7c4c4e
David Koeppl <david.koeppl75@gmail.com>
2025-04-14 17:10:53 +0200

    add back button

--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -3,10 +3,12 @@ import Page from '../components/Page.tsx';
 import Flex from '../components/Flex.tsx';
 import ProjectMarkdown from '../components/ProjectMarkdown.tsx';
 import ProjectMedia from '../components/ProjectMedia.tsx';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { projects } from '../assets/projects.ts';
 import { Project } from '../assets/project.types.tsx';
 import ProjectLinks from '../components/ProjectLinks.tsx';
+import { Button } from '../components/ui/button.tsx';
+import { ArrowLeftIcon } from '@radix-ui/react-icons';
 
 export function ProjectDetailPage() {
   const { slug } = useParams<{ slug: string }>();
@@ -18,6 +20,14 @@ export function ProjectDetailPage() {
     <Page title={project.title}>
       <Flex justify="center">
         <Flex direction="column" className={'lg:w-3/4 xl:w-1/2 mx-8'} gap={3}>
+
+          <Link to="/projects">
+            <Button variant="ghost" className="w-fit flex items-center gap-x-2 text-sm text-muted-foreground hover:text-foreground">
+              <ArrowLeftIcon className="w-4 h-4" />
+              Projects
+            </Button>
+          </Link>
+
           <ProjectLinks project={project} />
           <ProjectMarkdown markdown={project.markdown} />
           <ProjectMedia project={project as Project} />
