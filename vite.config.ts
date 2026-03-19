92a7810d59d5493d0f441723639c3eb1b5388b09
David Koeppl <david.koeppl75@gmail.com>
2026-03-19 09:17:31 +0100

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import react from '@vitejs/plugin-react';
 import { Mode, plugin as mdPlugin } from 'vite-plugin-markdown';
 
 export default defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
     react(),
-    tsconfigPaths(),
-    mdPlugin({ mode: [Mode.MARKDOWN]}),
+    mdPlugin({ mode: [Mode.MARKDOWN] }),
   ],
 });
